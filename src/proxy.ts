668577ee7238de0fa4a3bf2af0d28c2a00345011
lisboa-daniel <lisboa.daniel.dev@gmail.com>
2025-11-05 10:42:02 -0300

    feat: updated route protection

--- src/proxy.ts
+++ src/proxy.ts
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 import * as jwt from 'jsonwebtoken';
 
-export async function middleware(request: NextRequest) {
+export async function proxy(request: NextRequest) {
   const protectedRoutes = ['/catalog', '/profile', '/bookmarks'];
   
   const isProtectedRoute = protectedRoutes.some(route => 
@@ -15,19 +15,19 @@ export async function middleware(request: NextRequest) {
   
   const token = request.cookies.get('session')?.value;
   
-  console.log('🍪 Token:', token ? 'EXISTS' : 'MISSING');
+
   
   if (!token) {
-    console.log('❌ Redirecting - no token');
+
     return NextResponse.redirect(new URL('/signin', request.url));
   }
   
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET!);
-    console.log('✅ Token valid:', decoded);
+
     return NextResponse.next();
   } catch (err : any) {
-    console.log('❌ Token invalid:', err.message);
+    
     return NextResponse.redirect(new URL('/signin', request.url));
   }
 }
